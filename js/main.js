65d3f7a2998a87c5054623f20d7fb49039320f7c
Papp Zoltán <pzoli@honlapozz.hu>
2017-10-15 16:47:08 +0200

    jQuery 2.2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function fadeDone() {
 //Eseménykezelő beállítása
 $("p").click(function(){
     $(this).hide();
-    $(this).slideDown(3500);
+    $(this).slideDown(3500).css("color","blue");
 //    $(this).fadeTo(5000,1,fadeDone);
 });
 
@@ -17,9 +17,33 @@ $("p").click(function(){
  $("nav a.nav-link").click(function(ev){
     ev.preventDefault();
     var link = $(this);
-    $(document.body).animate({
-        opacity: '0'
-    }, 1000, function(){
+    var prop = link.data("prop") || "opacity";
+    var val = link.data("value") || "0";
+    var speed = link.data("speed") || 1000;
+    var settings = {}; //objektum létrehozása
+    settings[prop] = val; //animate első paramétere
+
+    $(document.body).animate(settings, speed, function(){
         document.location = link.attr("href");
     });
  });
+
+ //Events script
+ $(".events-search-row input").on("keyup",function(ev) {
+    $.each($(".events-card-deck .card .card-title"), function(index,elem){
+      var elem = $(elem);
+      var search = ev.target.value.toLowerCase();
+      var content = elem.html().toLowerCase();
+      if ( content.indexOf(search) == -1 ) {
+        elem.parents(".card").hide();
+      } else {
+        elem.parents(".card").show();
+      }
+    });
+  });
+
+// regiszter oldal
+$(".cherry-custom-filr").on("change", function(ev){
+    var name = ev.target.value.split("\\").pop();
+    $(this).find(".file-name").html(name);
+})
