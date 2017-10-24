e218bcd2b12cd86db53c51211f678d4ac5273d4b
Papp Zoltán <pzoli@honlapozz.hu>
2017-10-24 23:45:01 +0200

    ajax megoldas tanari

--- js/tickets-table-tanari.js
+++ js/tickets-table-tanari.js
@@ -1,76 +1,77 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var RESTURL = "http://localhost:3000";
     var searchString = '';
     var sortKey = '';
-    var sortDirection ='';
+    var sortDirection = '';
     var ticketListTable = $("#ticket-list");
-    //lapozáshoz globális változók
-    var pageLimit = 4; // hány egyed jelenjen meg
-    var currentPage = 1;
-    var maxPage = 0;
-    var totalCount = 0;
+    // lapozas globalis valtozoi
+    var pageLimit = 3; // hany egyed jelenjen meg egy lapon
+    var currentPage = 1; // jelenleg hol all a lapozo
+    var maxPage = 0; // hany oldalt tudunk megjeleniteni
+    var totalCount = 0; // osszes egyed szam amit a server tud szolgaltatni
 
-    // tábla kitöltése javascript objektumból 
+    // tabla kitoltese javascript object-bol
     function fillTicketsTable(currentTickets) {
         var tbody = $("#ticket-list tbody");
         tbody.html('');
 
         $.each(currentTickets, function (index, ticket) {
             var row = $(".templates .ticket-row").clone();
-//            row.find("td").eq(0).html(index+1);
-            row.find("td").eq(0).html(index+1);
+            row.find("td").eq(0).html(ticket.id);
             row.find("td").eq(1).html(ticket.event);
             row.find("td").eq(2).html(ticket.time);
             row.find("td").eq(3).html(ticket.seller);
             row.find("td").eq(4).html(ticket.pcs);
             row.find("td").eq(5).html(ticket.link);
             tbody.append(row);
-      });
+        });
     }
 
-    // lista újratöltése ajax-al 
-    // meghívja a fillTicketsTabele-t is
-    function refreshTicketList(){
+    // lista ujratoltese ajax-val (meghivja a fillTicketsTable-t is!)
+    function refreshTicketList() {
         var urlParams = [];
         var url = RESTURL + "/tickets";
 
-        // lapozó adatok kezelése
+        // lapozo adatok kezelese
         urlParams.push('_limit=' + pageLimit);
         urlParams.push('_page=' + currentPage);
-
-        // ez a sima szoveges kerese kezelese
-        if (searchString.length > 0){
-            urlParams.push('q='+searchString);
+        // sima szoveges kereses lekezelese
+        if (searchString.length > 0) {
+            urlParams.push('q=' + searchString);
         }
 
         // rendezes kezelese
-        if(sortKey.length > 0) {
+        if (sortKey.length > 0) {
             urlParams.push('_sort=' + sortKey);
             urlParams.push('_order=' + sortDirection);
-
         }
-        $("#ticket-list-paginator > ul > li").on('click', function(){
-            currentPage = $(this).text();
-         }); 
 
-        // ha van url parameter akkor osszefuzuk az url valtozoba
-        if(urlParams.length > 0) {
+        // ha van url parameter akkor osszefozzuk az url valtozoba
+        if (urlParams.length > 0) {
             url = url + "?" + urlParams.join('&');
         }
 
-        $.getJSON(url).done(function(ticketList, textStatus, request){
-            var oldMaxPage = maxPage;
-            totalCount = request.getResponseHeader('X-Total_Count');
-            maxPage = totalCount / pageLimit;
-            if (maxPage % 1 !== 0 ) {
-                maxPage = parseInt(maxPage) + 1;
+        $.getJSON(url).done(
+            function (ticketList, textStatus, request) {
+                var oldMaxPage = maxPage;
+                // valasz fejlecbol kiolvassuk az osszes lehetseges talalat szamat
+                totalCount = request.getResponseHeader('X-Total-Count');
+                maxPage = totalCount / pageLimit;
+                // modulus (maradekos) osztas
+                if (maxPage % 1 !== 0) {
+                    maxPage = parseInt(maxPage) + 1;
+                }
+                // ha valtozott az oldalak szama akkor ujra kirajzoljuk a lapozot
+                if (oldMaxPage != maxPage) {
+                    renderTicketTabletPaginator();
+                }
+
+                // lapozo ertekeinek frissitese
+                refreshPaginate();
+                // tablazat kirajzolasa az uj adatokkal
+                fillTicketsTable(ticketList);
             }
-            if (oldMaxPage != maxPage) {
-                renderTicketTablePaginator();
-            }
-            refreshPaginate();
-            fillTicketsTable(ticketList);
-        });
+        );
     }
 
     function refreshPaginate() {
@@ -108,49 +109,30 @@ $(document).ready(function(){
         paginatorElem.find('ul > li').eq(currentPage).addClass('active');
     }
 
-    /*    function refreshPaginate(){
-        var paginatorElem = $("#ticket-list-paginator");
-        var firstElem = paginatorElem.find("ul > li:first-child");
-        var lastElem = paginatorElem.find("ul > li:last-child");
-
-        if (currentPage == 1 ){
-            // bal oldali nyíl tiltása
-            firstElem.addClass('disabled');
-
-            lastElem.removeClass('disabled');
-            lastElem.prev().removeClass('disabled');
-        } else {
-            firstElem.removeClass('disabled');
-            firstElem.next().removeClass('disabled');
-            lastElem.addClass('disabled');
-
-        }
-        paginatorElem.find('ul > li').eq(currentPage).addClass('active');
-
-    } */
-
-
-    function renderTicketTablePaginator(){
-        var paginatorUlElem = $('#ticket-list-paginator > ul');
-        paginatorUlElem.html("");
+    function renderTicketTabletPaginator() {
+        var paginatorULElem = $('#ticket-list-paginator > ul');
+        // mivel ujra generaljuk a lapozot, ezert elotte uritjuk
+        paginatorULElem.html('');
 
         var html = [];
+        // balra nyilacska html (nem valtoztatjuk)
         html.push('<li class="page-item"><a class="page-link" href="#" aria-label="Previous" data-paginate-size="prev"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>');
-        // belső elemek 
-        for(var i=1; i <= maxPage; i++){
-            html.push('<li class="page-item"><a class="page-link" href="#" data-paginate-size="'+i+'">'+i+'</a></li>');      
+
+        for (var i = 1; i <= maxPage; i++) {
+            // a belso elemek toltese (szam elemek)
+            html.push('<li class="page-item"><a class="page-link" href="#" data-paginate-size="' + i + '">' + i + '</a></li>');
         }
 
+        // jobbra nyilacska html (nem valtoztatjuk)
         html.push('<li class="page-item"><a class="page-link" href="#" aria-label="Next" data-paginate-size="next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>');
-      
-        // tomb osszefuzese ures szoveggel es utana az UL elem-be toltese
-        paginatorUlElem.html(html.join(''));
 
+        // tomb osszefuzese ures szoveggel es utana az UL elem-be toltese
+        paginatorULElem.html(html.join(''));
         // mivel kicsereltuk a lapozo teljes html-t igy az esemenykezeloket is ujra hozza kell adni
         bindPaginatorEvents();
     }
 
-  /*
+    /*
      * erre itt azert van szukseg mert amikor renderTicketTabletPaginator() metodus lefut
      * akkor mi toroltuk az UL -ben levo osszes elemet igy az eddig felvett click 
      * esemenyek torlodtek es ujra fel kell hogy vegyuk oket mivel renderTicketTabletPaginator() -ban 
@@ -182,38 +164,38 @@ $(document).ready(function(){
         );
     }
 
-
-    $(".tickets-search-row input").on("keyup", function () {
-        searchString = $(this).val();
-        refreshTicketList();
-    });
-
-
-    ticketListTable.find("thead th[data-key]").on("click", function () {
-        var th = $(this);
-        $.each(ticketListTable.find('thead th[data-key]'), function (index, elem){
-            var currentTh = $(elem);
-            if (th.data("key") != currentTh.data("key")) {
-            currentTh.removeClass("asc").removeClass("desc");
-            }
-        });
-
-        sortKey = th.data("key");
-
-        
-        if(th.hasClass("asc")) {
-            sortDirection = 'desc';
-            th.removeClass("asc").addClass("desc");
-        } else {
-            sortDirection = 'asc';
-            th.removeClass("desc").addClass("asc");
-        }
+    // keres box lekezelese
+    $(".tickets-search-row input").on("keyup",
+        function () {
+            // feltoltjuk a "globalis" kereses szoveget tarolo valtozot
+            searchString = $(this).val();
+            // frissitjuk a listat
             refreshTicketList();
         });
 
+    // rendezes lekezelese (fejlecben kattintas hatasa)
+    ticketListTable.find("thead th[data-key]").on("click",
+        function () {
+            var th = $(this);
+            $.each(ticketListTable.find('thead th[data-key]'), function (index, elem) {
+                var currentTh = $(elem);
+                if (th.data("key") != currentTh.data("key")) {
+                    currentTh.removeClass("asc").removeClass("desc");
+                }
+            });
+            sortKey = th.data("key");
 
-    // innen indult az alkalmazás
+            if (th.hasClass("asc")) {
+                sortDirection = 'desc';
+                th.removeClass("asc").addClass("desc");
+            } else {
+                sortDirection = 'asc';
+                th.removeClass("desc").addClass("asc");
+            }
+
+            refreshTicketList();
+        });
+
+    // Innen indul az alkalmazas
     refreshTicketList();
-
-
 });
